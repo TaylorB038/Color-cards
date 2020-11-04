0fc6e823db91f5afdb384c08c341514623829c00
TaylorB038 <72984055+TaylorB038@users.noreply.github.com>
2020-11-04 20:05:00 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var config = document.querySelector(".config")
+
+config.addEventListener("click", configuracoes)
+
+function configuracoes() {
+  document.querySelector("div.config").style.display = "block"
+  document.querySelector("button").addEventListener ("click", zerar)
+  function zerar() {
+    localStorage.remove("melhor")
+  }
+}
