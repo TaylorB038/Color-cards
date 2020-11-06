846bcddcb47e0f0fd44755d794ddb07b9d4e447d
TaylorB038 <72984055+TaylorB038@users.noreply.github.com>
2020-11-06 10:34:12 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function abrir() {
   console.log("Funciona!")
-  
-  //document.querySelector(".configuracoes").style.display = "block"
-  //document.querySelector("button").addEventListener("click", zerar)
-  //function zerar() {
-  //  localStorage.remove("melhor")
+  document.querySelector(".configuracoes").style.display = "block"
+}
+document.querySelector("button").addEventListener("click", zerar)
+function zerar() {
+  localStorage.remove("melhor")
 }
