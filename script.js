3cb90ed22f59c4e01d290fa2d361eb5eedf79cda
TaylorB038 <72984055+TaylorB038@users.noreply.github.com>
2020-11-05 22:33:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var config = document.querySelector(".config")
 config.addEventListener("click", configuracoes)
 
 function configuracoes() {
-  document.querySelector("div.config").style.display = "block"
+  document.querySelector(".configuracoes").style.display = "block"
   document.querySelector("button").addEventListener("click", zerar)
   function zerar() {
     localStorage.remove("melhor")
