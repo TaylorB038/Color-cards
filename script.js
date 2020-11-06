3d898f842faaa598231dd9aa31376360a25d6284
TaylorB038 <72984055+TaylorB038@users.noreply.github.com>
2020-11-06 10:31:23 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-var config = document.querySelector(".config")
-
-config.addEventListener("click", abrir)
-
 function abrir() {
-  document.querySelector(".configuracoes").style.display = "block"
-  document.querySelector("button").addEventListener("click", zerar)
-  function zerar() {
-    localStorage.remove("melhor")
-  }
+  console.log("Funciona!")
+  
+  //document.querySelector(".configuracoes").style.display = "block"
+  //document.querySelector("button").addEventListener("click", zerar)
+  //function zerar() {
+  //  localStorage.remove("melhor")
 }
