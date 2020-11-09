396c526ac289a747b35656134a3bcb4d61265815
TaylorB038 <72984055+TaylorB038@users.noreply.github.com>
2020-11-09 16:40:20 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function abrir() {
-  document.querySelector(".conteinerC").style.display = "block"
-  document.querySelector(".T404").style.display = "block"
+  var T404 = document.querySelector(".T404")
+  T404.style.display = "flex"
+  document.querySelector("#conteinerC").style.display = "block"
 
-document.querySelector(".botaozerar").addEventListener("click", zerar)
-function zerar() {
-  localStorage.removeItem("melhor")
+  document.querySelector("#botaozerar").addEventListener("click", zerar)
+  function zerar() {
+    localStorage.removeItem("melhor")
+  }
+  
+  document.querySelector(".fechar").addEventListener("click", fechar)
+  function fechar() {
+    T404.style.display = "none"
+  }
 }
