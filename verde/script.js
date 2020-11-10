3bf3551c81e0a04f9d6b55d76e621700d642cfc2
TaylorB038 <72984055+TaylorB038@users.noreply.github.com>
2020-11-10 08:12:58 -0300

    update script.js

--- verde/script.js
+++ verde/script.js
@@ -232,7 +232,7 @@ function fail() {
   //salvar ou relacionar pontuação
   if (localStorage.getItem("melhor") < pontos || localStorage.getItem("melhor") == null) {
     localStorage.setItem("melhor", pontos)
-    document.querySelector(".conteinerF").innerHTML += `<p>Melhor Pontuação!</p>`
+    document.querySelector(".conteinerF").innerHTML += `<p class="best"><b>Melhor Pontuação!</b></p>`
   }else{
     document.querySelector(".best").style.display = "none"
   }
