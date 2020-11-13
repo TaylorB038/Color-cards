2078bdd985b4ef4ffe250a41e4fd645a6c1155b2
TaylorB038 <72984055+TaylorB038@users.noreply.github.com>
2020-11-12 21:34:26 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function abrir() {
+  //mostrar conteiner de configuraçoes
   var T404 = document.querySelector(".T404")
   T404.style.display = "flex"
   document.querySelector("#conteinerC").style.display = "block"
-
+  
+  //funçao do botão de reza zerar
   document.querySelector("#botaozerar").addEventListener("click", zerar)
   function zerar() {
     localStorage.removeItem("melhor")
   }
   
+  //função do nosso de fechar
   document.querySelector(".fechar").addEventListener("click", fechar)
   function fechar() {
     T404.style.display = "none"
   }
+}
+
+function play() {
+  document.querySelector(".cores").style.display = "flex"
 }
