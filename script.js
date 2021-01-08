292264c3b52711c46fbc0c357ac06c3499f13893
TaylorB038 <72984055+TaylorB038@users.noreply.github.com>
2021-01-08 20:25:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 function abrir() {
-  //mostrar conteiner de configuraçoes
   var T404 = document.querySelector(".T404")
-  T404.style.display = "flex"
-  document.querySelector("#conteinerC").style.display = "block"
+  var fechar = document.querySelector(".fechar")
+  var cont = document.querySelector(".conteinerConfig")
   
-  //funçao do botão de reza zerar
-  document.querySelector("#botaozerar").addEventListener("click", zerar)
+  //mostrar conteiner de configuraçoes
+  T404.style.display = "block"
+  cont.style.display = "block"
+  
+  //funçao do botão de limpar dados
+  document.querySelector(".limpardados").addEventListener("click", zerar)
   function zerar() {
     localStorage.removeItem("melhor")
   }
+  fechar.addEventListener("click", close)
   
-  //função do nosso de fechar
-  document.querySelector(".fechar").addEventListener("click", fechar)
-  function fechar() {
+  //função de fechar
+  function close() {
     T404.style.display = "none"
+    cont.style.display = "none"
   }
 }
 
 function play() {
   var tela = document.querySelector(".tela")
-  var cores = document.querySelector(".telacores")
+  var cores = document.querySelector(".selectcor")
   tela.style.display = "none"
   cores.style.display = "flex"
   
   var topleft = document.querySelector(".topleft")
   topleft.style.zIndex = 4
+  document.querySelector(".topleft, img").setAttribute("src", "media/fechar.svg")
+  
   topleft.addEventListener("click", function() {
     cores.style.display = "none"
     tela.style.display = "flex"
   })
 }
+
+function mudo() {
+  sessionStorage(som, "desl")
+}
+
+var pontos = localStorage.getItem("melhor")
+//document.querySelector(".melhor").innerHTML += `<p>${pontos}</p>`;
