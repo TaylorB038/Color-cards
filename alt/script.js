5f7b18379542bbc4a00cdf0d252defbf9072c4e6
TaylorB038 <72984055+TaylorB038@users.noreply.github.com>
2021-01-09 14:11:31 -0300

    Delete script.js

--- alt/script.js
+++
@@ -1,9 +0,0 @@
-function abrir() {
-  document.querySelector(".T404").style.display = "flex"
-  document.querySelector("#conteinerC").style.display = "block"
-
-  document.querySelector("#botaozerar").addEventListener("click", zerar)
-  function zerar() {
-    localStorage.removeItem("melhor")
-  }
-}
